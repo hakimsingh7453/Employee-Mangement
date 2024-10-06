1ab403ca68dcee24543ea44b9592af8154bdb640
manish70372038 <mrjackel88@gmail.com>
2024-10-06 15:13:33 +0530

    i have added style file and navbar modified file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,22 @@
+import './style.css';
+
+
 function Navbar() {
     return (
       <div>
-        <h1>navbar for manish</h1>
+        
+    <nav className="name">
+         
+          <ul className="contain">
+              <a href="#">HOME </a>
+             <a href="#">SERCVICES </a>
+              <a href="#"> CONTECT US  </a>
+              <a href="#"> PROFILES</a>
+          </ul>
+         
+    </nav>
+     
+         
       </div>
     );
   }

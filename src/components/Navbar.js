05b171bc9807bb37441773e480b936c40d6114a0
manish70372038 <mrjackel88@gmail.com>
2024-10-06 15:50:51 +0530

    i have refracter the code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,17 @@
 import './style.css';
 
-
 function Navbar() {
-    return (
-      <div>
-        
-    <nav className="name">
-         
-          <ul className="contain">
-              <a href="#">HOME </a>
-             <a href="#">SERCVICES </a>
-              <a href="#"> CONTECT US  </a>
-              <a href="#"> PROFILES</a>
-          </ul>
-         
-    </nav>
-     
-         
-      </div>
-    );
-  }
-  
-  export default Navbar;
+  return (
+    <div>
+      <nav className="name">
+        <ul className="contain">
+          <a href="#">HOME </a>
+          <a href="#">SERCVICES </a>
+          <a href="#"> CONTECT US  </a>
+          <a href="#"> PROFILES</a>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
